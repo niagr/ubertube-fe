175c21a27985636ec2b2a81ef5c19d5524929808
Nishant <nishantagrwal12993@gmail.com>
2018-09-05 03:46:34 +0530

    Added sample API call

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-class Main extends React.Component {
+class Main extends React.Component<any, any> {
+
+    constructor (props: any) {
+        super(props)
+        this.state = {}
+    }
+
+    componentDidMount () {
+        this.getVideos()
+    }
+
+    async getVideos () {
+        const data = await fetch('http://localhost:8000/scratchpad').then((r:any) => r.json())
+        this.setState({data})
+    }
+
+    componentDidCatch (e: Error) {
+        this.setState({error: e})
+    }
+
     render () {
-        return <div>Hello!</div>
+        const {data=null, error} = this.state
+        if (error) {
+            return <div>Error</div>
+        }
+        return <div style={{whiteSpace: 'pre'}}>Hello!: <br/>{JSON.stringify(data, null, 2)}</div>
     }
 }
 
